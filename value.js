5105e40a07a0cef6496ea6d892255a55e0de4ed0
Ankit Singh <ankits19783@gmail.com>
2026-04-12 14:36:27 +0530

    condom page added

--- value.js
+++ value.js
@@ -1,8 +1,10 @@
 function getvalue(){
     let input = String(document.getElementById("input").value);
     let toupperinput = input.toUpperCase();
-    if(toupperinput === "SMARTWATCH" || toupperinput === ("SMART WATCH")){
+    if(toupperinput === "SMARTWATCH" || toupperinput === "SMART WATCH"){
         window.location = "smartwatch.html"
+    }else if(toupperinput === "CONDOM"){
+        window.location = "condom.html"
     }else{
         window.location = "index.html"
     }
